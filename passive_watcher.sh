951342a89aa19c631c2efba5b173a32617abeaee
izcet <isaacjrhett@gmail.com>
2018-01-25 13:23:13 -0800

    [passive_watcher.sh]

--- passive_watcher.sh
+++ passive_watcher.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-LDIR="logs"
-LFIL="latest.log"
-LOGS="$LDIR/$LFIL"
+LOG_DIR="logs"
+LOG_FILE="latest.log"
+LOGS="$LOG_DIR/$LOG_FILE"
 
 TEMP="/tmp/.mcap_$(head -c 20 /dev/random | base64)"
 
 OUT_FILE="mcap_list.txt"
+
+REGEX_MACOS="s/............Server.thread.INFO]: ([[:alnum:]_]{1,}) (joined|left) the game/\1 \2/"
+REGEX_LINUX=""
 
 
 mkdir -p "$TEMP"
@@ -28,23 +31,30 @@ trap on_exit EXIT
 
 # add_player name
 function add_player () {
-	return
-	#exit
+	PLAYER="$1"
+	for P in "${ARR[@]}" ; do
+		if [ "$P" == "$PLAYER" ] ; then
+			return
+		fi
+	done
+	ARR[${ARR[@]}]="$PLAYER"
 }
+
 # remove_player name
 function remove_player () {
-	return
-	#exit
+	PLAYER="$1"
+	INDEX=0
+	while [ "$INDEX" -lt "${#ARR[@]}" ] ; do
+		if [ "${ARR[$INDEX]}" == "$PLAYER" ] ; then
+			unset ARR[$INDEX]
+		else
+			((INDEX++))
+		fi
+	done
 }
 
-
-
-
-
-
-REGEX_MACOS="s/............Server.thread.INFO]: ([[:alnum:]_]{1,}) (joined|left) the game/\1 \2/"
-REGEX_LINUX=""
 function parse_logs () {
+	echo -n "Parsing logs... "
 	while read LINE ; do
 		PLAYER="$(echo "$LINE" | sed -E "$REGEX_MACOS")"
 		if [ "$PLAYER" != "$LINE" ] ; then
@@ -57,23 +67,32 @@ function parse_logs () {
 			else
 				echo "whoops"
 			fi
-			echo "$PLAYER"
-			echo "$ACTION"
 		fi
-	done < "$TEMP/$LFIL"
+	done < "$TEMP/$LOG_FILE"
+	echo "Done!"
 }
 
-parse_logs
+function update_file () {
+	echo -n "Updating file... "
+	if [ "${#ARR[@]}" -eq "0" ] ; then
+		echo "(no players currently online)" > "$OUT_FILE"
+	else
+		echo "" > "$OUT_FILE"
+		for PLAYER in "${ARR[@]}" ; do
+			echo "$PLAYER" >> "$OUT_FILE"
+		done
+	
 
-exit
+	echo "Done!"
+}
 
 ###################################################################################################
 while [ 1 ] ; do
-
-	if [ -n "$(diff "$TEMP/$LFIL" "$LOGS")" ] ; then
+	if [ -n "$(diff "$TEMP/$LOG_FILE" "$LOGS")" ] ; then
 		echo "log files differ"
 		cp "$LOGS" "$TEMP"
 		parse_logs
+		update_file 
 	fi
 
 
